a833c05b977068a6220c2dcafae88cd7f9c0e9fd
Rxjpawar <rxjpawar@gmail.com>
2026-02-19 18:46:19 +0530

    Refactor memory configuration; streamline LLM setup and remove commented-out sections for clarity

--- app/mem_config.py
+++ app/mem_config.py
@@ -10,36 +10,36 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 config = {
     "version": "v1.1",
+    "llm": {
+        "provider": "groq",
+        "config": {
+            "api_key": GROQ_API_KEY,
+            "model": "meta-llama/llama-4-scout-17b-16e-instruct",
+        },
+     },
     # "llm": {
-    #     "provider": "groq",
+    #     "provider": "openai",
+    #     "config": {"api_key": OPENAI_API_KEY, "model": "gpt-4o-mini"},
+    # },
+    # # vector embedder
+    # "embedder": {
+    #     "provider": "openai",
+    #     "config": {"api_key": OPENAI_API_KEY, "model": "text-embedding-3-small"},
+    # },
+    # # vector memory
+    # "vector_store": {
+    #     "provider": "qdrant",
+    #     "config": {"host": "localhost", "port": "6333"},
+    # },
+    # # relationships  - missing part of my life
+    # "graph_store": {
+    #     "provider": "neo4j",
     #     "config": {
-    #         "api_key": GROQ_API_KEY,
-    #         "model": "meta-llama/llama-4-scout-17b-16e-instruct",
+    #         "url": "bolt://localhost:7687",
+    #         "username": "neo4j",
+    #         "password": "reform-william-center-vibrate-press-5829",
     #     },
     # },
-    "llm": {
-        "provider": "openai",
-        "config": {"api_key": OPENAI_API_KEY, "model": "gpt-4o-mini"},
-    },
-    # vector embedder
-    "embedder": {
-        "provider": "openai",
-        "config": {"api_key": OPENAI_API_KEY, "model": "text-embedding-3-small"},
-    },
-    # vector memory
-    "vector_store": {
-        "provider": "qdrant",
-        "config": {"host": "localhost", "port": "6333"},
-    },
-    # relationships  - missing part of my life
-    "graph_store": {
-        "provider": "neo4j",
-        "config": {
-            "url": "bolt://localhost:7687",
-            "username": "neo4j",
-            "password": "reform-william-center-vibrate-press-5829",
-        },
-    },
 }
 
 mem_client = Memory.from_config(config)
