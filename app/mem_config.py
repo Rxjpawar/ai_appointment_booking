e7ec4f32252fb990ef852d22425eb665807e7726
Rxjpawar <rxjpawar@gmail.com>
2026-02-20 11:44:36 +0530

    Refactor memory configuration; remove commented-out sections for clarity and maintainability

--- app/mem_config.py
+++ app/mem_config.py
@@ -17,29 +17,14 @@ config = {
             "model": "meta-llama/llama-4-scout-17b-16e-instruct",
         },
      },
-    # "llm": {
-    #     "provider": "openai",
-    #     "config": {"api_key": OPENAI_API_KEY, "model": "gpt-4o-mini"},
-    # },
-    # # vector embedder
-    # "embedder": {
-    #     "provider": "openai",
-    #     "config": {"api_key": OPENAI_API_KEY, "model": "text-embedding-3-small"},
-    # },
-    # # vector memory
-    # "vector_store": {
-    #     "provider": "qdrant",
-    #     "config": {"host": "localhost", "port": "6333"},
-    # },
-    # # relationships  - missing part of my life
-    # "graph_store": {
-    #     "provider": "neo4j",
-    #     "config": {
-    #         "url": "bolt://localhost:7687",
-    #         "username": "neo4j",
-    #         "password": "reform-william-center-vibrate-press-5829",
-    #     },
-    # },
+    "graph_store": {
+        "provider": "neo4j",
+        "config": {
+            "url": "bolt://localhost:7687",
+            "username": "neo4j",
+            "password": "reform-william-center-vibrate-press-5829",
+        },
+    },
 }
 
 mem_client = Memory.from_config(config)
